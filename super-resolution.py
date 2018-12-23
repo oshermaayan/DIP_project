60466991dcdaac153945ad6bff934b7fec6a149a
Osher Maayan <t-osmaay@microsoft.com>
2018-12-23 18:23:14 +0200

    Now supporting saving visual results during training

--- super-resolution.py
+++ super-resolution.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 
 import argparse
 import os
+import time
 # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 import numpy as np
@@ -33,8 +34,8 @@ use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 
 torch.backends.cudnn.enabled = True
-torch.backends.cudnn.benchmark =True
-dtype = torch.cuda.FloatTensor
+torch.backends.cudnn.benchmark = True
+dtype = torch.cuda.FloatTensor#torch.FloatTensor
 
 '''List parameters:
     factor - 4 or 8
@@ -62,17 +63,21 @@ parser.add_argument('--net_arch', type=str, help='CNN architecture. Currently su
 parser.add_argument('--network_depth', type=int, help='How many layers the CNN contains (in skip-connections: the number\n'
                                                        ' of layers is doubled - one for "down" direction and on for "up" directrion'
                                                        , default=32)
-parser.add_argument('--displ_freq', type=int, help='In how many iterations the results will be displayed', default=1000)
+parser.add_argument('--disp_freq', type=int, help='In how many iterations the results will be displayed', default=1000)
 
 parameters = parser.parse_args()
 
+img_name = parameters.file_path.split('/')[-1]
+results_dir = "results/sr/"+img_name+"/"
+if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
 
 
 imsize = -1 
 factor = parameters.factor #4#88
 enforse_div32 = 'CROP' # we usually need the dimensions to be divisible by a power of two (32 in this case)
 PLOT = True
-plot_frequency = parameters.displ_freq #100
+plot_frequency = parameters.disp_freq #100
 
 # To produce images from the paper we took *_GT.png images from LapSRN viewer for corresponding factor,
 # e.g. x4/zebra_GT.png for factor=4, and x8/zebra_GT.png for factor=8 
@@ -186,7 +191,7 @@ downsampler = Downsampler(n_planes=3, factor=factor, kernel_type=KERNEL_TYPE, ph
 
 def closure():
     global i, net_input
-    
+    start_time = time.time()
     if reg_noise_std > 0:
         ### Add noise to network - the bigger the SR factor, the more noise (higher std) is added!
         net_input = net_input_saved + (noise.normal_() * reg_noise_std)
@@ -214,10 +219,15 @@ def closure():
     psnr_history.append([psnr_LR, psnr_HR])
     
     if PLOT and i % plot_frequency == 0:
+        img_path = results_dir+"iter_"+str(i)+"_"+parameters.net_arch+\
+                   "depth_"+str(parameters.network_depth)+"_"+parameters.weight_init+".jpg"
+        torchvision.utils.save_image(out_HR,img_path)
         out_HR_np = torch_to_np(out_HR)
         plot_image_grid([imgs['HR_np'], imgs['bicubic_np'], np.clip(out_HR_np, 0, 1)], factor=13, nrow=3)
 
     i += 1
+    end_time = time.time() #TODO: remove
+    print("Closure function duration is ",(end_time-start_time))
     
     return total_loss
 

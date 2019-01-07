f5580bd47004377e1de9b164877a4cfa3f5e278d
osherm <comet.star.catcher@gmail.com>
2019-01-07 16:51:48 +0200

    fixed poisson input noise creation

---
+++ running_zeroRegNoise4000Iter.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+/usr/bin/python3.5 /home/osherm/PycharmProjects/deep-image-prior/super-resolution.py --file_path data/sr/zebra_GT.png --net_arch skip --weight_init kaiming_uniform --reg_noise_zero true --iter_num 4000
+/usr/bin/python3.5 /home/osherm/PycharmProjects/deep-image-prior/super-resolution.py --file_path data/sr/zebra_GT.png --net_arch skip --weight_init kaiming_normal --reg_noise_zero true --iter_num 4000
